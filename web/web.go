25a40c1e92a28e3fef9bc9ac578c22e5510e5ba0
CHEN Feng <chen3feng@gmail.com>
2026-04-24 15:36:16 +0000

    refactor: remove hardcoded CipherSuites, let Go TLS library choose defaults

--- web/web.go
+++ web/web.go
@@ -196,14 +196,6 @@ func (s *Server) Start() (err error) {
 		c := &tls.Config{
 			Certificates: []tls.Certificate{cert},
 			MinVersion:   tls.VersionTLS12,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-			},
 		}
 		listener = network.NewAutoHttpsListener(listener)
 		listener = tls.NewListener(listener, c)
